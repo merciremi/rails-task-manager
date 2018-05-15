1cbc470be2d37f11eaead7cbc82390bb4441f2e7
Remi Mercier <remi.merci@gmail.com>
2018-05-15 13:58:11 +0200

    Add post verb to new task logic

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,14 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
 
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 
 end
