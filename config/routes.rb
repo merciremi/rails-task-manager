e8946a19b6c3b2b11aacaec1f3fecabca0e64aa8
Remi Mercier <remi.merci@gmail.com>
2018-05-15 13:14:32 +0200

    Add new task logic to controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'tasks#index', as: :tasks
 
+  get '/tasks/new', to: 'tasks#new', as: :new
+  post '/', to: 'tasks#create'
+
   get '/tasks/:id', to: 'tasks#show', as: :task
+
 
 
 end
